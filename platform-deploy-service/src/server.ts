4a37c39dbf47400ed17c2e0a82a8b1ee4254c806
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2026-07-31 20:02:40 -0400

    Fix platform operation step lookup filters

--- platform-deploy-service/src/server.ts
+++ platform-deploy-service/src/server.ts
@@ -584,8 +584,8 @@ async function getOperation(operationId: string): Promise<PlatformOperation> {
 async function getActiveOperationForApp(appId: string): Promise<PlatformOperation | null> {
   const params = new URLSearchParams();
   params.set("fields", "id,operation_type,status");
-  params.set("filter[app_id][_eq]", appId);
-  params.set("filter[status][_in]", "queued,running");
+  params.set("filter[_and][0][app_id][_eq]", appId);
+  params.set("filter[_and][1][status][_in]", "queued,running");
   params.set("sort", "-date_created");
   params.set("limit", "1");
   const response = await directusJson<DirectusListResponse<PlatformOperation>>(
@@ -647,8 +647,8 @@ function optionalInt(value: unknown): number | undefined {
 async function getOperationStep(operationId: string, stepKey: string): Promise<PlatformOperationStep | null> {
   const params = new URLSearchParams();
   params.set("fields", "id,operation_id,app_id,step_key,status,result_json");
-  params.set("filter[operation_id][_eq]", operationId);
-  params.set("filter[step_key][_eq]", stepKey);
+  params.set("filter[_and][0][operation_id][_eq]", operationId);
+  params.set("filter[_and][1][step_key][_eq]", stepKey);
   params.set("limit", "1");
   const response = await directusJson<DirectusListResponse<PlatformOperationStep>>(
     `/items/platform_app_operation_steps?${params.toString()}`
