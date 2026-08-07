178df042bc9070c0ec375f67fffeb4243051fc9d
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2026-08-06 21:08:01 -0400

    removing unused line
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- platform-deploy-flink-job/src/main/java/com/suncoast/platform/deploy/flink/PlatformDeployJob.java
+++ platform-deploy-flink-job/src/main/java/com/suncoast/platform/deploy/flink/PlatformDeployJob.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 
